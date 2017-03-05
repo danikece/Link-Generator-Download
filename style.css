87205b64c482e6a07f79a7ba6c5144548a791007
danikece <daniblogger852@gmail.com>
2017-03-04 18:48:24 -0800

    Add files via upload

---
+++ style.css
@@ -0,0 +1,69 @@
+body {
+  padding-bottom: 50px;
+  margin: 0;
+}
+
+h1 {
+  margin: 50px auto 15px;
+  font-weight: 700;
+  font-family: 'Open Sans', Helvetica, Arial, sans-serif;
+}
+
+h3 {
+  font-size: 16px;
+}
+
+#page_content {
+  text-align: center;
+}
+
+form {
+  width: 80%;
+  padding: 20px 0 0!important;
+  display: inline-block!important;
+  text-align: center;
+}
+
+.example {
+  font-weight: normal;
+}
+
+img {
+  width: auto;
+  max-width: 100%;
+  height: auto;
+}
+
+.ex-text {
+  color: #ccc;
+}
+
+#driveID {
+  width: 300px;
+  margin: 0 auto;
+}
+
+.get {
+  padding: 0 20px;
+}
+
+#container {
+  display: none;
+}
+
+#link_container,
+.get-another {
+  margin-top: 20px;
+}
+
+#driveID {
+  margin-top: 20px;
+}
+
+.how-to {
+  text-align: left;
+  margin-top: 30px;
+}
+.footer {
+  margin-top: 50px;
+}
